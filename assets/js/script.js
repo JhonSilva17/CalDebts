ae88a43b7be2412f7aa109f69cdefd5aeaad36f9
JhonSilva17 <jhon.dev.silva@gmail.com>
2022-05-22 00:26:16 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,126 @@
+// Variáveis das transações
+const itemValue = document.querySelector('#item-value')
+const itemName = document.querySelector('#item-name')
+
+// Função que criará os cards de transações
+const ulOuput = document.querySelector('.list-items')
+let transactions = JSON.parse(localStorage.getItem('transactions')) || []
+let values = JSON.parse(localStorage.getItem('values')) || []
+
+// Função que desenvolve a lista com os nomes e os valores das transações
+function atualizaTela() {
+    ulOuput.innerHTML = '';
+
+    transactions.forEach((item, index)=> {
+        // Criando o item da lista
+        let li = document.createElement('li')
+        li.classList.add('item')
+        ulOuput.appendChild(li)
+
+        // Criando a div 
+        let div = document.createElement('div')
+        div.classList.add('items-data')
+        li.appendChild(div)
+
+        // criando a tag do nome da transação
+        let spanName = document.createElement('span')
+        let textSpanName = document.createTextNode(item)
+        spanName.appendChild(textSpanName)
+        spanName.classList.add('color-dark')
+        div.appendChild(spanName)
+
+        // criando a tag do valor da transação
+        let spanValue = document.createElement('span')
+        let spanValueText = document.createTextNode(formatCurrency(Number(values[index])))
+        spanValue.classList.add(values[index] < 0 ? 'color-red' : 'color-green')
+        spanValue.appendChild(spanValueText)
+        div.appendChild(spanValue)
+
+        li.addEventListener('click', ()=> {
+            const confirmation = confirm('Tem certeza que quer remover sua transação?')
+            // Caso o 'confirmation' for true, o código abaixo será executado
+            if (confirmation) {
+                ulOuput.removeChild(li)
+                remove(transactions, item) // Removendo os itens clicados da lista 
+                remove(values, values[index]) // Removendo os itens clicados da lista 
+                saveStorage('Transactions', transactions) // Salvando as remoções no localStorage
+            }
+            cardsValues()
+        })
+    })
+}
+atualizaTela() // Sempre que a página recarrega, a função é ativada
+
+// Função que atualiza os cards com os valores
+const profit = document.querySelector('#ganhos')
+const expense = document.querySelector('#despesas')
+const total = document.querySelector('#total')
+
+// Função que efetua os cálculos e atualiza as informações nos cards superiores da aplicação
+function cardsValues() {
+    let somaGanhos = 0
+    let somaExpense = 0
+
+    values.forEach(item => {
+        if (item > 0) {
+            somaGanhos = somaGanhos + item
+        } else if (item < 0) {
+            somaExpense = somaExpense + item
+        }
+    })
+
+    let somaTotal = somaGanhos - (somaExpense * -1)
+
+    localStorage.setItem('somaGanhos', formatCurrency(somaGanhos))
+    localStorage.setItem('somaExpense', formatCurrency(somaExpense))
+    localStorage.setItem('somaTotal', formatCurrency(somaTotal))
+
+    profit.innerHTML = localStorage.getItem('somaGanhos')
+    expense.innerHTML = localStorage.getItem('somaExpense')
+    total.innerHTML = localStorage.getItem('somaTotal')
+
+}
+cardsValues() // Sempre que a página recarrega, a função é ativada
+
+// Função que formata o número em formato da moeda brasileira
+function formatCurrency(value){
+    return value.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
+}
+
+// Função que remove itens de um determinada array
+function remove(arr, item) {
+    // arr.indexOf(item): retorna a posição do item no array
+    // arr.splice(arr.indexOf(item, 1)) - está removendo o item na posição informada(do item) e remove apenas 1 elemento apartir da posição do item
+    arr.splice(arr.indexOf(item), 1)
+
+    atualizaTela() // Atualizo a tela com os valores removidos
+    saveStorage() // Salvo as remoções no localStorage
+}
+
+// Função que salva os arrays de dados no localStorage
+function saveStorage() {
+    // Definindo uma chave para o array transactions, transformei todo o array em String JSON.(Pois o local storage só armazena o tipo de dado string)
+    localStorage.setItem('transactions', JSON.stringify(transactions))
+    localStorage.setItem('values', JSON.stringify(values))
+}
+
+const buttonSend = document.querySelector('#btnSend')
+// Callback que executa as funções quando o botão é acionado
+buttonSend.addEventListener('click', (event)=> {
+    event.preventDefault() // Impedindo que o botão recarregue a página após o clique
+    
+    // Estou verificando se os campos estão vazios, se estiver eu nao executo o script.
+    if (itemValue.value == '' || itemName.value == '') {
+        alert('Você deve preencher todos os campos.')
+    } else {
+        transactions.push(itemName.value) // Adicionando ao array transactions, o valor digitado no input 'itemName'
+        values.push(Number(itemValue.value)) // Adicionando ao array values, o valor digitado no input 'itemValue'
+        cardsValues() // Quando o botão é clicado a função é chamado
+        atualizaTela() // Quando o botão é clicado a função é chamado
+        saveStorage() // Quando o botão é clicado a função é chamado
+        
+        // Ao atualizar a tela com os dados, os inputs são limpos
+        itemName.value = ''
+        itemValue.value = ''
+    }
+})
